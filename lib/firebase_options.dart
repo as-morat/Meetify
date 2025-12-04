b93ccacfa185b05ee32be251d304f9cf3595163e
as-morat <morat9511@gmail.com>
2025-12-04 16:03:20 +0600

    update security issues

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -49,20 +50,21 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAvfRZfXtbonnqYTVP3FlGFVHpEWRAktMA',
-    appId: '1:58501890498:android:40cceba51724bcb895165b',
-    messagingSenderId: '58501890498',
-    projectId: 'meetify-646',
-    storageBucket: 'meetify-646.firebasestorage.app',
+
+  static FirebaseOptions get android => FirebaseOptions(
+    apiKey: dotenv.env['ANDROID_API_KEY']!,
+    appId: dotenv.env['ANDROID_APP_ID']!,
+    messagingSenderId: dotenv.env['SENDER_ID']!,
+    projectId: dotenv.env['PROJECT_ID']!,
+    storageBucket: dotenv.env['STORAGE_BUCKET']!,
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyDgMRUBfwsugAxSciAIweUUPSKXHHEKXns',
-    appId: '1:58501890498:ios:d62520e2af1b11ef95165b',
-    messagingSenderId: '58501890498',
-    projectId: 'meetify-646',
-    storageBucket: 'meetify-646.firebasestorage.app',
-    iosBundleId: 'com.example.meetify',
+  static FirebaseOptions get ios => FirebaseOptions(
+    apiKey: dotenv.env['IOS_API_KEY']!,
+    appId: dotenv.env['IOS_APP_ID']!,
+    messagingSenderId: dotenv.env['SENDER_ID']!,
+    projectId: dotenv.env['PROJECT_ID']!,
+    storageBucket: dotenv.env['STORAGE_BUCKET']!,
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID'],
   );
 }
